240accf19bb8ab1fdb35c4ade25956a7c57c3b91
Clinton Kanyali <clintkanyali@gmail.com>
2024-08-10 22:33:05 -0400

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -7,30 +7,6 @@
 echo "**Welcome to our C5_Calculator**"
 echo "================================"
 
-Err = ""
-
-echo "Please enter first number" #entering the first number
-read first_number
-
-while [[ ! $first_number =~ ^[0-9]+$ ]] #testing if the input was an integer
-do
-    echo "Not a number. Try again"
-    read first_number
-done
-    echo "Your first number is $first_number"
-    echo " "
-
-echo "Please enter second number"  #entering the second number
-read second_number
-
-while [[ ! $second_number =~ ^[0-9]+$ ]] #testing if the input was an integer
-do
-    echo "Not a number. Try again"
-    read second_number
-done
-    echo "Your second number is $second_number"
-    echo " "
-
 echo "Which operation do you want to do?" #Providing arithmetic operations as prompts
 echo "Press/Enter a number 1 to 4 and press enter where;"
 echo "1 - Addition"
@@ -40,31 +16,49 @@ echo "4 - Division"
 echo " "
 
 read operation_number #inputing the selected operation
+if [ $operation_number -eq 4 ]; then
+  echo -e "\e[31m!! Please note that your first integer is the numerator and the second number is the denominator !!\e[0m"
+fi
+
+echo "Please enter first integer" #entering the first number
+read first_number
+
+while [[ ! $first_number =~ ^-?[0-9]+$ ]] #testing if the input was an integer
+do
+    echo "Not an integer. Please enter an integer"
+    read first_number
+done
+    echo "Your first integer is $first_number"
+    echo " "
+
+echo "Please enter second integer"  #entering the second number
+read second_number
+
+while [[ ! $second_number =~ ^-?[0-9]+$ ]] #testing if the input was an integer
+do
+    echo "Not an integer. Please enter an integer"
+    read second_number
+done
+    echo "Your second intger is $second_number"
+    echo " "
+
 
 if [ $operation_number -eq 1 ]; then #if input was addition or +
-  echo "The sum of those two numbers is; " $(($first_number + $second_number))
+  echo "The sum of those two numbers is " $(($first_number + $second_number))
 
   elif [ $operation_number -eq 2 ]; then  #if input was subtraction or -
     echo "The difference between " "$first_number - $second_number = "$(($first_number - $second_number)) #first way to subtract
-    echo "or"
-    echo "The difference between " "$second_number - $first_number = "$(($second_number - $first_number)) #second way to subtract
-
+    
   elif [ $operation_number -eq 3 ]; then #if input was multiplication x
-    echo "The multiplication of those two numbers is; " $(($first_number * $second_number))
+    echo "The product between " "$second_number x $first_number is "$(($second_number * $first_number))
 
-  elif [ $operation_number -eq 4 ]; then  #if input was division or /
+  elif [ $operation_number -eq 4 ]; then  #if input was division or / #1 & #5 | #5 & #1
 	if [ $second_number -ne 0 ]; then #checking if the denominator is zero
-	#first way to divide while giving a remainder as an integer too
-	  echo "The division between " "$first_number / $second_number = "$(($first_number / $second_number)) "remainder" $(($first_number % $second_number))
-	elif [ $first_number -eq 0 ]; then #checking if the denominator is zero
-	#second way to divide while giving a remainder as an integer too
-	   echo "The division of zeros is impossible"
-	elif [ $first_number -ne 0 ]; then #checking if the denominator is zero
-	   echo "The division between " "$second_number / $first_number = "$(($second_number / $first_number)) "remainder" $(($second_number % $first_number))
+	   echo "The division between " "$first_number / $second_number = "$(($first_number / $second_number)) "with a remainder of" $(($first_number % $second_number))
 	else
-	  #the error when the denominator is zero
-	   echo "The division of between " "$second_number / $first_number is impossible - as you can't divide a number by zero "
-	 fi
+ 	   echo "The division of "$first_number / $second_number = " is impossible"
+	fi
 else
   echo "You haven't selected a proper option mathematical option"
+  
 fi
